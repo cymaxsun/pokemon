01fe47b146ab91d13aec81715ef4da32f52b37af
cymax <cymax@DESKTOP-2U8K4BG>
2024-01-11 14:18:59 -0500

    forgot this

---
+++ Summative/src/moves/Harden.java
@@ -0,0 +1,31 @@
+package moves;
+
+import main.ApplicationData;
+import pokemon.Pokemon;
+import pokemon.PokemonTypes;
+
+public class Harden extends PokemonMove{
+
+	
+	public Harden() {
+		super();
+		setName("Harden");
+		setType(PokemonTypes.NORMAL);
+		setPower(0);
+		setMaxCharges(30);
+		setAcc(100);
+		setSFX(getClass().getResource("/sound/harden.wav"));
+	}
+	
+	
+	public void useMove(Pokemon attacker, Pokemon target) {
+		if (charges <= 0) {
+			useStruggle(attacker, target);
+			return;
+		}
+		
+		super.useMove(attacker, target);
+		attacker.setDefStage(attacker.getDefStage()+1);
+	}
+	
+}
